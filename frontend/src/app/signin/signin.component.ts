23639ce46d7700b81b61d97663734fb3580dfda0
Justin James <digitaldrummerj@gmail.com>
2017-04-29 19:03:24 -0700

    implement minLength check in TypeScript

--- frontend/src/app/signin/signin.component.ts
+++ frontend/src/app/signin/signin.component.ts
@@ -10,12 +10,22 @@ import { Router } from '@angular/router';
 export class SigninComponent implements OnInit {
   loginInvalid: boolean = false;
   constructor(private authService: AuthService, private router: Router) { }
-
+  private minLength: number = 6;
+  public minLengthError: boolean = false;
   ngOnInit() {
   
   }
 
   login(formValues) {
+    let password: string = formValues.password;
+    
+    if (password.length < this.minLength){
+      this.minLengthError = true;
+      this.loginInvalid = false;
+      return;
+    }
+
+    this.minLengthError = false;
     this.authService.login(formValues.email, formValues.password)
       .subscribe(result => {
         if (!result) {
