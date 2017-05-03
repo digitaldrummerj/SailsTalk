9493174282f781243c408555811b23d8cb7ba13e
Justin James <digitaldrummerj@gmail.com>
2017-05-03 08:49:17 -0700

    add full path to chrome

--- reset.bat
+++ reset.bat
@@ -14,9 +14,9 @@ cmd.exe /c run-angular.bat
 cmd.exe /c code backend
 
 cmd.exe /c C:\Users\user\AppData\Local\Postman\Update.exe --processStart "Postman.exe"
-cmd.exe /c  chrome http://node-machine.org/machinepack-emailaddresses
-cmd.exe /c  chrome http://node-machine.org/machinepack-passwords
-cmd.exe /c  chrome http://node-machine.org/machinepacks
+cmd.exe /c  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://node-machine.org/machinepack-emailaddresses
+cmd.exe /c  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://node-machine.org/machinepack-passwords
+cmd.exe /c  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://node-machine.org/machinepacks
 
 REM must be last since it start interactive session
 cmd.exe /c run-sailspostgres.bat
